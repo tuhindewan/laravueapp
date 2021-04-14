37de7dafca46273e7cb8fb8056473eda6e46f840
tuhindewan <tuhinsshadow@gmail.com>
2021-04-14 23:37:20 +0600

    Instant search

--- resources/js/app.js
+++ resources/js/app.js
@@ -84,8 +84,8 @@ const app = new Vue({
         search: '',
     },
     methods:{
-        searchit(){
+        searchit: _.debounce(() => {
             Fire.$emit('searching')
-        }
+        }, 1000)
     }
 });
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -21,7 +21,7 @@
       <div class="form-inline">
         <div class="input-group" data-widget="sidebar-search">
           <input class="form-control form-control-sidebar"
-                type="search" v-model="search" @keyup.enter="searchit"
+                type="text" v-model="search" @keyup="searchit"
                 placeholder="Search" aria-label="Search">
           <div class="input-group-append">
             <button class="btn btn-sidebar">
